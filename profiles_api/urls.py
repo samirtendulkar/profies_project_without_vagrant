03a6ce3449c13ce326b9e88cc97740ed5c31662f
Samir <samnik12345@gmail.com>
2018-10-20 10:55:30 -0400

    Added seacrh function to UserProfilesApi

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -6,9 +6,11 @@ from . import views
 router = DefaultRouter()
 router.register("hello-viewset", views.HelloViewSet, base_name="hello-viewset")
 router.register("profile", views.UserProfileViewSets)
+router.register("login", views.LoginViewSet, base_name="login")
 
 
 urlpatterns = [
     url(r'^hello-view/$', views.HelloApiView.as_view(), name="hello_view"),
     url(r'', include(router.urls))
 ]
+
